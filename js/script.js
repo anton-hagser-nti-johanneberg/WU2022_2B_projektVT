616f905961da7363aeb45a1826597f0b41728d90
Anton Hagsér <anton.hagser@epsidel.se>
2022-05-27 17:06:49 +0200

    add additional content on contact-us and about-us page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Fetch required elements on load
+// Fetch required elements
 mobileNavBar = document.getElementById("hamburger-menu");
 hamburgerMenu = document.getElementById("hamburger-icon");
 navItems = document.getElementById("nav-items");
@@ -11,6 +11,7 @@ function toggleHamburgerMenu() {
 
 // Toggle hamburger on click
 hamburgerMenu.addEventListener("click", (event) => {
+    hamburgerMenu.classList.toggle("active");
     toggleHamburgerMenu();
 });
 
@@ -29,5 +30,22 @@ function checkSizing() {
     }
 }
 
-// Check sizing on load
-checkSizing();
+// Register on load event and initialize the map
+window.addEventListener("load", (event) => {
+    // Check sizing on load
+    checkSizing();
+
+    // Check if mapboxgl has loaded (it's only imported on the contact-us page)
+    // a better way of doing this is per page scripting and then compressing it
+    if (typeof mapboxgl !== "undefined") {
+        // Set the public access token
+        mapboxgl.accessToken =
+            "pk.eyJ1Ijoic3RhcnRvbGVmdCIsImEiOiJjamtmcnhwdGIwYnR6M3NxbjR3cTBvdThjIn0.fihE9-g-sm0dgoMksATtUw";
+
+        // Create the map and set a style
+        var map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/mapbox/streets-v8",
+        });
+    }
+});
